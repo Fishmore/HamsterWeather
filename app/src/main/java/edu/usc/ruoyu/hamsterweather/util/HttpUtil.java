39da6d41acae4d3597b857817b0d3528a54879b9
Ruoyu <ruoyuli@usc.edu>
2017-07-11 01:29:11 -0700

    List Province City and County

---
+++ app/src/main/java/edu/usc/ruoyu/hamsterweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package edu.usc.ruoyu.hamsterweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Ruoyu on 2017/7/10.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
